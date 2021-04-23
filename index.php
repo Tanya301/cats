e46c6f7c3bcf8d82f31c52d75c8e13be3e27a6a5
Tanya S <tmp@MacBook-Air-3.local>
2021-04-23 09:42:01 +0300

    added cat guardians

--- index.php
+++ index.php
@@ -6,9 +6,16 @@
 
 <body class='mainStl'>
   <table class='tabl'>
-    <tr>
+    <td align="center">
+      <img class="mw-100" src="img\csMir.png" width="300" height="400">
+    </td>
+    <td align="center">
       <br><b>Welcome :)</b><br><br>
-      <button onclick="location.href = 'mainPage.php';" id=mainPageBtn class='btn'>To the main page</button>
-    </tr>
+      <button onclick="location.href = 'mainPage.php';" id=mainPageBtn class='btn'>To the main page</button><br><br>
+      <button onclick="location.href = 'auth.php';" id=authBtn class='btn'>Authtorize</button>
+    </td>
+    <td align="center">
+      <img class="mw-100" src="img\cs.png" width="300" height="400">
+    </td>
 </body>
 </html>
