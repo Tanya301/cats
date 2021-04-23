00a028c64673ae14a6c3ee31ab4ecbc6dd6c2e33
Tanya S <tmp@MacBook-Air-3.local>
2021-04-23 15:20:42 +0300

    first step to getting done with connecting to db normally

--- dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $conn = mysql_connect('localhost', 'root', '');
-  mysql_select_db('cats');
-  if (!$conn) {
-    die('error connecting to db' . mysql_error());
-  }
-
-  echo 'Connected to db "cats" . <br>';
-?>
