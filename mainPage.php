91d2dc13e38f3266af036aba7310aca3f2b4416c
Tanya S <tmp@MacBook-Air-3.local>
2021-04-30 08:16:44 +0300

    now with sql query :)

--- mainPage.php
+++ mainPage.php
@@ -6,12 +6,39 @@
 
 <body class='mainStl'>
   <button onclick="location.href = 'index.php';" id=mainPageBtn class='btn'>Back home</button><br><br>
-  <?php
-    session_start(); 
-    include "dbcon.php";
-    $result = mysql_query('select * from cats');
-    $row = mysql_fetch_row($result);
-    echo $row[0];
-  ?>
+
+  <table class="tabl" border=1>
+    <tr>
+      <th>id</th>
+      <th>Name</th>
+      <th>breed_id</th>
+      <th>description</th>
+      <th>color_id</th>
+      <th>user_id</th>
+    </tr>
+
+
+    <?php
+    $conn = mysqli_connect("localhost", "root", "root", "cats");
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT * FROM cats";
+
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["id"]. "</td><td>" . $row["name"] . "</td><td>". $row["breed_id"]. "</td> <td>". $row["description"]. "</td><td>". $row["color_id"]. "</td><td>". $row["user_id"]. "</td></tr>";
+    }
+      echo "</table>";
+    } 
+    else { 
+      echo "0 results"; 
+    }
+
+    $conn->close();
+    ?>
+  </table>
+  
 </body>
 </html>
