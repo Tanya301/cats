2ae5dec408a2c17a77d7c89c0e4d99bcb8839879
Tanya S <tmp@MacBook-Air-3.local>
2021-04-30 15:07:42 +0300

    modified about page, added signup.php(without auth yet)

--- about.php
+++ about.php
@@ -5,9 +5,10 @@
 </head>
 
 <body class='mainStl'>
-  <img class="mw-100" src="img/about.jpeg" height="400"><br><br>
+  <div class="headr">About</div><br>
+  <img class="mw-100" src="img/about.jpeg" height="400"><img class="mw-100" src="img/stles.jpeg" height="400"><img class="mw-100" src="img/aboutMir.jpeg" height="400"><br>
   <p>Данный проект просто необходим человечеству, он позволяет решить главную проблему в современном мире -- недостаток котиков. Недостаток котиков действительно является важной проблемой, ведь мы подвержены такому большому количеству стлеса, и кто как не котики способен это исправить</p>
-  <img class="mw-100" src="img/stles.jpeg" height="400"><br><br>
+  <br><br>
   <button onclick="location.href = 'index.php';" id=mainPageBtn class='btn'>Back home</button><br><br>
 </body>
 </html>
---
+++ signup.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <link rel='stylesheet' type='text/css' href='stlz.css'>
+  <title>Sign Up</title>
+</head>
+
+<body class='mainStl'>
+  <img class="mw-100" src="img/authCat.jpeg" width="400" height="300"><br><br>
+  <button onclick="location.href = 'index.php';" id=mainPageBtn class='btn'>Back home</button>     
+  <button onclick="location.href = 'auth.php';" id=authBtn class='btn'>Back to Auth</button><br><br>
+
+
+</body>
+</html>
