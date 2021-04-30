3f93f46d368b7c193acd03a50c4bfba5a3df331b
Tanya S <tmp@MacBook-Air-3.local>
2021-04-30 10:57:03 +0300

    added a style for sql table -- doesnt apply for some reason

--- mainPage.php
+++ mainPage.php
@@ -4,10 +4,10 @@
   <title>CATS</title>
 </head>
 
-<body class='mainStl'>
+<body class="mainStl">
   <button onclick="location.href = 'index.php';" id=mainPageBtn class='btn'>Back home</button><br><br>
 
-  <table class="tabl" border=1>
+  <table class='tableMainPage'> <!--width=70% text-align=center background-color=#ff3bf8 border=1 align=center>-->
     <tr>
       <th>id</th>
       <th>Name</th>
@@ -17,28 +17,24 @@
       <th>user_id</th>
     </tr>
 
-
     <?php
-    $conn = mysqli_connect("localhost", "root", "root", "cats");
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-    $sql = "SELECT * FROM cats";
+      $conn = mysqli_connect("localhost", "root", "root", "cats");
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+      $sql = "SELECT * FROM cats";
 
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["id"]. "</td><td>" . $row["name"] . "</td><td>". $row["breed_id"]. "</td> <td>". $row["description"]. "</td><td>". $row["color_id"]. "</td><td>". $row["user_id"]. "</td></tr>";
-    }
-      echo "</table>";
-    } 
-    else { 
-      echo "0 results"; 
-    }
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+          echo "<tr><td>" . $row["id"]. "</td><td>" . $row["name"] . "</td><td>". $row["breed_id"]. "</td> <td>". $row["description"]. "</td><td>". $row["color_id"]. "</td><td>". $row["user_id"]. "</td></tr>";
+        }
+      } else { 
+        echo "0 results"; 
+      }
 
-    $conn->close();
+      $conn->close();
     ?>
   </table>
-  
 </body>
 </html>
