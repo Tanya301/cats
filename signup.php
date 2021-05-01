6b49585442734d4ce951eac3b75226c38e12066c
Tanya S <tmp@MacBook-Air-3.local>
2021-05-01 21:56:12 +0300

    played with the signup page a bit

--- signup.php
+++ signup.php
@@ -5,10 +5,20 @@
 </head>
 
 <body class='mainStl'>
-  <img class="mw-100" src="img/paw.png" width="400" height="300"><br><br>
-  <button onclick="location.href = 'index.php';" id=mainPageBtn class='btn'>Back home</button>     
-  <button onclick="location.href = 'auth.php';" id=authBtn class='btn'>Back to Auth</button><br><br>
+  <div class="headr">Join our community</div><br>
+  <table class='tabl'>
+    <td>
+      <img class="mw-100" src="img/pawMir.png" width="400">
+    </td>
+    <td>
 
+      <!--here will be the form-->
 
+      <button onclick="location.href = 'index.php';" id=mainPageBtn class='btn'>Back home</button>     
+      <button onclick="location.href = 'auth.php';" id=authBtn class='btn'>Back to Auth</button>
+    </td>
+    <td>
+      <img class="mw-100" src="img/paw.png" width="400">
+    </td>
 </body>
 </html>
