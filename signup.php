ef6a31d93d2938c7079b3869533cd937bc0c0d85
Tanya S <tmp@MacBook-Air-3.local>
2021-05-01 21:48:57 +0300

    minor changes

--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@
 </head>
 
 <body class='mainStl'>
-  <img class="mw-100" src="img/authCat.jpeg" width="400" height="300"><br><br>
+  <img class="mw-100" src="img/paw.png" width="400" height="300"><br><br>
   <button onclick="location.href = 'index.php';" id=mainPageBtn class='btn'>Back home</button>     
   <button onclick="location.href = 'auth.php';" id=authBtn class='btn'>Back to Auth</button><br><br>
 
